19b87ef42ae99b96dd6dc9b729d7da705a9f7394
lalithameenakshiv <lalithameenakshiv66@gmail.com>
2026-02-10 10:41:19 +0530

    Add digit reversal program in jp2.reverse.java
    Implement a program to reverse digits of an integer.

---
+++ jp2.reverse.java
@@ -0,0 +1,20 @@
+3.//reversal digit
+PROGRAM
+import java.util.*;
+   public class Main {
+    public static void main(String[] args) {
+      Scanner scan= new Scanner(System.in);
+    int a = scan.nextInt();
+    int rev = 0;
+    while(a>0){
+      int r = a%10;
+    rev=rev*10+r;
+    a=a/10;
+    
+    }
+      System.out.println(rev);
+      
+    }
+}
+output
+987

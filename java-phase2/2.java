04e80dc1a3ca9d8965a72a73cc66e42466d9cd3f
lalithameenakshiv <lalithameenakshiv66@gmail.com>
2026-02-10 12:29:04 +0530

    Update 2.java

--- java-phase2/2.java
+++ java-phase2/2.java
@@ -106,3 +106,29 @@ PROGRAM
 OUTPUT
  not a palindrome 
 
+5.// samll to caps
+PROGRAM
+  import java.util.*;
+
+public class Main {
+  
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      char ch = scan.next().charAt(0);
+      if(ch>='A' && ch<='Z'){
+        System.out.println((char)(ch+32));
+      }
+      else if(ch>='a' && ch<='z'){
+        System.out.println((char)(ch-32));
+    }
+    else{
+      System.out.println(ch); 
+    }
+      }
+     
+    
+}
+
+OUTPUT
+  S
+

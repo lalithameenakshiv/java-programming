d24c18b64a8ee13bbbcfd4b410c63e2d7b981527
lalithameenakshiv <lalithameenakshiv66@gmail.com>
2026-02-10 14:36:30 +0530

    Update 2.java

--- java-phase2/2.java
+++ java-phase2/2.java
@@ -218,5 +218,25 @@ public class Main {
  }
 OUTPUT
  not prime 
+
+//10.finding factorial
+PROGRAM
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+        int mul = 1;
+
+        for (int i = 1; i <= num; i++) {
+            mul = mul * i;
+        }
+
+        System.out.println(mul);
+    }
+}
+OUTPUT
+  720
       
        

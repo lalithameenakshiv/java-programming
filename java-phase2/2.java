3ab1060aab025c353d6722030e3825d1eea0d3b2
lalithameenakshiv <lalithameenakshiv66@gmail.com>
2026-02-10 16:02:31 +0530

    Update 2.java

--- java-phase2/2.java
+++ java-phase2/2.java
@@ -260,6 +260,27 @@ public class  Main{
     }
 }
 OUTPUT
- Enter number of terms: 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 
+ Enter number of terms: 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765
+
+//12.base power
+PROGRAM
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base = sc.nextInt();
+        int exponent = sc.nextInt();
+        int result = 1;
+
+        for(int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println(result);
+    }
+}
+OUTPUT
+1679616
       
        

0922f0f83d22644325903a53ddd451d32324d9eb
lalithameenakshiv <lalithameenakshiv66@gmail.com>
2026-02-11 11:48:45 +0530

    Implement array input and output in Main class
    Added a Java program to read and print an array of integers.

--- java-phase2/2.java
+++ java-phase2/2.java
@@ -284,5 +284,20 @@ OUTPUT
 1679616
 
 //13.
+ import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int size = sc.nextInt();
+      int arr[]= new int [size];
+      for(int i =0;i<size;i++){
+        arr[i]=sc.nextInt();
+      }
+      for(int i=0;i<size;i++){
+        System.out.print(arr[i]+"");
+      }
+      
+    }
+} 
       
        

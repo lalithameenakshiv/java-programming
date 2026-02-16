b9274fb7e3e3dc1a8c9ae303e2572097b0445100
lalithameenakshiv <lalithameenakshiv66@gmail.com>
2026-02-16 14:34:22 +0530

    Update 2.java

--- java-phase2/2.java
+++ java-phase2/2.java
@@ -660,6 +660,35 @@ public class Main {
     }
 }
 
+25.selection sort
+  import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];   
+        
+        for(int i = 0; i < n; i++){   
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+          int minindex=i;
+          for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minindex]){
+              minindex=j;
+            }
+          }
+          int temp = arr[i];
+          arr[i]=arr[minindex];
+          arr[minindex]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+}
+}
+
+//26.
+
 
 
 

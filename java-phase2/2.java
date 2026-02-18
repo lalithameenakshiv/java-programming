7e8459df6b35068f3e286c3a0a316671d966d978
lalithameenakshiv <lalithameenakshiv66@gmail.com>
2026-02-18 12:32:02 +0530

    Update 2.java

--- java-phase2/2.java
+++ java-phase2/2.java
@@ -962,5 +962,115 @@ public class main{
 }
 }
 //35.
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class SinglyLinkedList {
+    int size = 0;
+    Node head = null;
+    Node tail = null;
+
+    // Add at end
+    void add(int data) {
+        Node newnode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newnode;
+        } else {
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
+
+    // Add at beginning
+    void addAtBeg(int data) {
+        Node newnode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newnode;
+        } else {
+            newnode.next = head;
+            head = newnode;
+        }
+    }
+
+    // Add at specific position
+    void addAtPos(int data, int pos) {
+
+        if (pos <= 0 || pos > size + 1) {
+            System.out.println("Please enter a valid position");
+            return;
+        }
+
+        if (pos == 1) {
+            addAtBeg(data);
+            return;
+        }
+
+        if (pos == size + 1) {
+            add(data);
+            return;
+        }
+
+        Node newnode = new Node(data);
+        size++;
+
+        Node temp = head;
+
+        for (int i = 1; i < pos - 1; i++) {
+            temp = temp.next;
+        }
+
+        newnode.next = temp.next;
+        temp.next = newnode;
+    }
+
+    // Display list
+    void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        SinglyLinkedList s = new SinglyLinkedList();
+
+        s.add(100);
+        s.add(200);
+        s.add(300);
+
+        s.addAtBeg(400);
+        s.display();
+
+        s.addAtPos(500, 3);
+        s.display();
+
+        s.addAtPos(600, 1);
+        s.display();
+
+        s.addAtPos(700, 2);
+        s.display();
+
+        System.out.println("Size of list: " + s.size);
+    }
+}
+
 
 
